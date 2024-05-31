acd30e7669ce6147f5a46454e26765402b50a3ba
Charles Renan Retes <retescharlesrenan@gmail.com>
2024-05-31 17:47:49 +0800

    Design - Icons Added

--- src/assets/icons/icons.js
+++ src/assets/icons/icons.js
@@ -1,7 +1,13 @@
 import discordWhite from "./discord-white.svg";
+import plusGreen from "./plus-green.svg";
+import downloadGreen from "./download-green.svg";
+import discoverGreen from "./discover-green.svg";
 
 const icons = {
   discordWhite,
+  plusGreen,
+  downloadGreen,
+  discoverGreen,
 };
 
 export default icons;
