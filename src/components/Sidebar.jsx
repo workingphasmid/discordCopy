e71c058edcdc3b19ffb62bc459c8e1d098b6bb45
Charles Renan Retes <retescharlesrenan@gmail.com>
2024-05-31 18:00:11 +0800

    Design - Separator

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,14 +2,17 @@ import React from "react";
 import Servers from "../components/Sidebar/Servers";
 import Item from "./Sidebar/Item";
 import icons from "../assets/icons/icons.js";
+import Separator from "./Sidebar/Separator.jsx";
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
       <Item icon={icons.discordWhite} />
+      <Separator />
       <Servers />
       <Item icon={icons.plusGreen} />
       <Item icon={icons.discoverGreen} />
+      <Separator />
       <Item icon={icons.downloadGreen} />
     </div>
   );
---
+++ src/components/Sidebar/Separator.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Separator = () => {
+  return <div className="separator"></div>;
+};
+
+export default Separator;
