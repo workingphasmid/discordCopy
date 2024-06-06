597fe483844c2ae2216b7e8e0b6826385976dc20
Charles Renan Retes <retescharlesrenan@gmail.com>
2024-06-06 14:02:30 +0800

    Added icons.

--- src/assets/icons/icons.js
+++ src/assets/icons/icons.js
@@ -6,6 +6,9 @@ import dropdownGrey from "./dropdown-grey.svg";
 import dropdownGrey200 from "./dropdown-grey-200.svg";
 import hashtagGrey200 from "./hashtag-grey-200.svg";
 import soundGrey200 from "./sound-grey-200.svg";
+import headphoneGrey300 from "./headphone-grey-300.svg";
+import micRed400 from "./mic-red-400.svg";
+import settingGrey300 from "./setting-grey-300.svg";
 
 const icons = {
   discordWhite,
@@ -16,6 +19,9 @@ const icons = {
   dropdownGrey200,
   hashtagGrey200,
   soundGrey200,
+  headphoneGrey300,
+  micRed400,
+  settingGrey300,
 };
 
 export default icons;
--- src/components/Base/SidebarChannels.jsx
+++ src/components/Base/SidebarChannels.jsx
@@ -1,13 +1,27 @@
 import React from "react";
 import Hat from "./SidebarChannels/Hat";
 import Shirt from "./SidebarChannels/Shirt";
+import icons from "../../assets/icons/icons.js";
 
 const SidebarChannels = () => {
   return (
     <div className="sidebar-channels">
       <Hat />
       <Shirt />
-      <div className="sidebar-channels__shoes">shoes</div>
+      <div className="sidebar-channels__shoes">
+        <div className="sidebar-channels__profile">
+          <img className="sidebar-channels__profile-picture" />
+          <div className="sidebar-channels__profile-wrapper">
+            <p className="sidebar-channels__name">blob</p>
+            <p className="sidebar-channels__status">invisible</p>
+          </div>
+        </div>
+        <div className="sidebar-channels__shoes-controls">
+          <img src="" alt="" />
+          <img src="" alt="" />
+          <img src="" alt="" />
+        </div>
+      </div>
     </div>
   );
 };
