b18dbb355960c426b1750a9ee0913b45971cc740
Charles Renan Retes <retescharlesrenan@gmail.com>
2024-06-06 17:47:09 +0800

    D - Fix font sizes.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import SidebarServers from "./components/SidebarServers";
 import Base from "./components/Base";
 import "./css/SidebarChannels.css";
+import "./css/Chat.css";
 
 function App() {
   return (
