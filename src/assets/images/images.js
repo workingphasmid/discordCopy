5d81ea1fbd12c340b08e6765e018ef1359c0e7a2
Charles Renan Retes <retescharlesrenan@gmail.com>
2024-06-06 14:42:02 +0800

    Design - Profile.

--- src/assets/images/images.js
+++ src/assets/images/images.js
@@ -1,9 +1,11 @@
 import channel1 from "./channel-1.jpg";
 import channel2 from "./channel-2.jpg";
+import profile from "./profile.jpg";
 
 const images = {
   channel1,
   channel2,
+  profile,
 };
 
 export default images;
--- src/components/Base/SidebarChannels.jsx
+++ src/components/Base/SidebarChannels.jsx
@@ -3,6 +3,8 @@ import Hat from "./SidebarChannels/Hat";
 import Shirt from "./SidebarChannels/Shirt";
 import icons from "../../assets/icons/icons.js";
 
+import images from "../../assets/images/images.js";
+
 const SidebarChannels = () => {
   return (
     <div className="sidebar-channels">
@@ -10,16 +12,22 @@ const SidebarChannels = () => {
       <Shirt />
       <div className="sidebar-channels__shoes">
         <div className="sidebar-channels__profile">
-          <img className="sidebar-channels__profile-picture" />
+          <img className="sidebar-channels__profile-picture" src={images.profile} />
           <div className="sidebar-channels__profile-wrapper">
-            <p className="sidebar-channels__name">blob</p>
+            <p className="sidebar-channels__name">Blob</p>
             <p className="sidebar-channels__status">invisible</p>
           </div>
         </div>
         <div className="sidebar-channels__shoes-controls">
-          <img src={icons.micRed400} alt="" />
-          <img src={icons.headphoneGrey300} alt="" />
-          <img src={icons.settingGrey300} alt="" />
+          <div className="sidebar__channels__control-container">
+            <img src={icons.micRed400} alt="" />
+          </div>
+          <div className="sidebar__channels__control-container">
+            <img src={icons.headphoneGrey300} alt="" />
+          </div>
+          <div className="sidebar__channels__control-container">
+            <img src={icons.settingGrey300} alt="" />
+          </div>
         </div>
       </div>
     </div>
